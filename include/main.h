d4614ea6134438d215779efedfe49157d7be5053
Tom McCarthy <tom@tomm.cc>
2025-03-30 12:17:23 +1000

    Apply HQ mixer

--- include/main.h
+++ include/main.h
@@ -53,6 +53,7 @@ extern u16 gKeyRepeatContinueDelay;
 extern bool8 gSoftResetDisabled;
 extern IntrFunc gIntrTable[];
 extern u8 gLinkVSyncDisabled;
+extern u32 IntrMain_Buffer[];
 extern s8 gPcmDmaCounter;
 
 void AgbMain(void);
